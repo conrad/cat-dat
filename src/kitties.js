ce1d0c7d38e8d11970b6509eeedc49bd176f8025
Devin Finzer <dfinzer2@gmail.com>
2017-12-10 16:22:07 -0800

    More reference code

--- src/kitties.js
+++ src/kitties.js
@@ -1,5 +1,6 @@
 var Web3 = require('web3');
 var axios = require('axios');
+var timebucket = require('timebucket');
 
 var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/" + process.env.INFURA_ENDPOINT));
 var contractAddress = "0xb1690C08E213a35Ed9bAb7B318DE14420FB57d8C"
@@ -11,5 +12,30 @@ axios.get('http://api.etherscan.io/api?module=contract&action=getabi&address=' +
     var events = KittyAuctionContract.getPastEvents("AuctionSuccessful", {fromBlock: 4711040, toBlock: 4711060}).then(function(sales) {
       console.log(sales);
     });
+  }
+});
+
+// Refernce code
+
+var searchQuery = "gen:2"
+axios.get("https://api.cryptokitties.co/auctions", {
+  params: {
+    offset: 2000,
+    limit: 100,
+    type: "sale",
+    status: "closed",
+    search: searchQuery,
+    sorting: "cheap",
+    orderBy: "currentPrice",
+    parents: false
+  }
+}).then(function(response) {
+  var auctions = response.data.auctions
+  for (var i = 0; i < auctions.length; i++) {
+    var saleStart = timebucket(parseInt(auctions[i].start_time))
+    saleStart = saleStart.resize('h')
+    var saleEnd = timebucket(parseInt(auctions[i].start_time))
+    saleEnd = saleEnd.resize('d')
+    console.log(saleEnd + "")
   }
 });
